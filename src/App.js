a64a6866cdcbe4f94806013026a940c3981d4038
Trond Aalberg <trond.aalberg@ntnu.no>
2023-04-12 00:44:00 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Unstable_Grid2';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+//import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import { red } from '@mui/material/colors';
-import { CardActionArea } from '@mui/material';
+//import { CardActionArea } from '@mui/material';
 import Stack from '@mui/material/Stack';
-import CardActions from '@mui/material/CardActions';
+//import CardActions from '@mui/material/CardActions';
 import CardHeader from '@mui/material/CardHeader';
-import Button from '@mui/material/Button';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+//import Button from '@mui/material/Button';
+//import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Work from "./Work";
@@ -27,8 +27,6 @@ import {
 import * as PropTypes from "prop-types";
 
 const queryClient = new QueryClient();
-
-const query = "http://localhost:7200/repositories/grouping-displays?query=PREFIX%20rdfs%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%20PREFIX%20rdac%3A%20%3Chttp%3A%2F%2Frdaregistry.info%2FElements%2Fc%2F%3E%20PREFIX%20rdf%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%20select%20%3Fs%20(SAMPLE(%3Flabel)%20as%20%3Fl)%20where%20%7B%20%20%09%3Fs%20rdf%3Atype%20rdac%3AC10001%20.%20%20%20%20%20OPTIONAL%7B%3Fs%20rdfs%3Alabel%20%3Flabel%7D.%20%7D%20%20group%20by%20%3Fs%20order%20by%20%3Fs%20limit%20100&infer=true";
 
 const uri = "http://localhost:7200/repositories/grouping-displays?query=";
 
@@ -43,9 +41,8 @@ const workSparql = `
     }
     group by ?s
     order by ?s
-    limit 100
+    limit 5
 `;
-
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -88,33 +85,9 @@ export default function App() {
     );
 }
 
-function WorkComp(props) {
-    return <Card style={{"flex-shrink": 0}}>
-        <CardContent>
-            <CardHeader
-                avatar={
-                    <Avatar sx={{bgcolor: red[500]}} aria-label="recipe">
-                        W
-                    </Avatar>
-                }
-                action={
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon/>
-                    </IconButton>
-                }
-                title={props.entry.l.value}
-                subheader={props.entry.s.value}
-            />
-        </CardContent>
-
-    </Card>;
-}
-
-WorkComp.propTypes = {entry: PropTypes.any};
-
 function Example() {
     const { isLoading, error, data } = useQuery({
-        queryKey: ['repoData'],
+        queryKey: ['workList'],
         queryFn: () =>
             fetch(uri + encodeURIComponent(workSparql), {headers: {
         "Accept": "application/sparql-results+json",
@@ -127,7 +100,6 @@ function Example() {
     if (isLoading) return 'Loading...'
 
     if (error) return 'An error has occurred: ' + error.message
-
     return (
         <Stack spacing={1} style={{maxHeight: "700px", overflow: 'auto'}}>{
             data.results.bindings.map(entry => (
--- src/Work.js
+++ src/Work.js
@@ -1,30 +1,97 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
+import Collapse from '@mui/material/Collapse';
 import Avatar from "@mui/material/Avatar";
 import {red} from "@mui/material/colors";
-import IconButton from "@mui/material/IconButton";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import * as React from "react";
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import * as PropTypes from "prop-types";
+import {useQuery} from "@tanstack/react-query";
+import Stack from "@mui/material/Stack";
+import CardActions from '@mui/material/CardActions';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+Work.propTypes = {entry: PropTypes.any};
 
 export default function Work(props){
+
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return(
         <Card key={props.entry.s.value} style={{'flex-shrink': 0}}>
-            <CardContent>
-                <CardHeader
-                    avatar={
-                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                            W
-                        </Avatar>
-                    }
-                    action={
-                        <IconButton aria-label="settings">
-                            <MoreVertIcon />
-                        </IconButton>
-                    }
-                    title={props.entry.l.value}
-                    subheader={props.entry.s.value}
-                />
+            <CardContent >
+                <Typography variant={"span"}>{props.entry.l.value}</Typography>
+                <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </ExpandMore>
             </CardContent>
+
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <WorkProperties wid={props.entry.s.value}/>
+            </Collapse>
         </Card>
     )}
+
+function WorkProperties(props) {
+
+    const uri = "http://localhost:7200/repositories/grouping-displays?query=";
+    const sparql = `
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        select * where { 
+            <${props.wid}> ?p ?o.
+            ?p rdfs:label ?x .
+            FILTER(LANG(?x) = "gden") 
+        }
+        limit 100 
+    `;
+
+    //console.log(sparql);
+
+    const { isLoading, error, data } = useQuery({
+        queryKey: [props.wid],
+        queryFn: () =>
+            fetch(uri + encodeURIComponent(sparql), {headers: {
+                    "Accept": "application/sparql-results+json",
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                }}).then(
+                (res) => {return res.json();},
+            ),
+    })
+
+    if (isLoading) return 'Loading...'
+
+    if (error) return 'An error has occurred: ' + error.message
+    return (
+        <CardContent>{
+            data.results.bindings.map(entry => (
+                <Typography paragraph>{entry.x.value + ": " + entry.o.value}</Typography>
+            ))
+        }</CardContent>
+    )
+}
